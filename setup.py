108e1ec44827015ce0aeb3d1df9b4cc325b36b6a
段洪义 <duanhongyi@zhumengyuan.com>
2018-07-20 16:09:03 +0000

    add setup

---
+++ setup.py
@@ -0,0 +1,47 @@
+#coding:utf-8
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+readme_file = os.path.join(here, 'README.md')
+
+def read_text(file_path):
+    """
+    fix the default operating system encoding is not utf8.
+    """
+    if sys.version_info.major < 3:
+        with open(file_path) as f:
+            return f.read()
+    with open(file_path, encoding="utf8") as f:
+        return f.read()
+
+README = read_text(os.path.join(here, 'README.md'))
+
+requires = [
+
+]
+
+test_requirements = [
+
+]
+
+
+setup(
+
+    name='genius',
+    description='genius中文分词 Chinese Segment On linear-chain CRF',
+    version='3.1.7',
+    author='duanhongyi',
+    author_email='duanhyi@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=README + '\n\n' + CHANGES,
+    url='https://github.com/duanhongyi/genius',
+    install_requires=requires,
+    tests_require=test_requirements,
+    platforms='all platform',
+    license='BSD',
+)
