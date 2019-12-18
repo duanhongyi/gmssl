05d50b87ba13b096d165d4d721434ce09d78e7dc
于博 <yu_bo@126.com>
2019-12-18 10:27:03 +0800

    增加 pytest 对应测试用例

--- tests/test_sm3.py
+++ tests/test_sm3.py
@@ -1,5 +1,33 @@
-from gmssl import sm3, func
+
+import random
+
+for i in range(1):
+    if i:
+        from gmssl.libsm3 import sm3
+    else:
+        from gmssl import sm3_implement as sm3
+
+    def test_sm3():
+        y = sm3.sm3_hash(b"abc")
+        assert y.upper() == "66C7F0F462EEEDD9D1F2D46BDC10E4E24167C4875CF2F7A2297DA02B8F4BA8E0"
+
+
+    def test_sm3_loop(basic_loop, rand_bytes):
+        sm3_hash = sm3.SM3()
+        sm3_hash.start()
+        msg_total = b''
+
+        for _ in range(basic_loop):
+            msg = rand_bytes(random.randint(1, 128))
+            sm3_hash.update(msg)
+            msg_total += msg
+
+        print(sm3.sm3_hash(msg_total))
+        assert sm3_hash.finish() == sm3.sm3_hash(msg_total)
+
+#66C7F0F462EEEDD9D1F2D46BDC10E4E24167C4875CF2F7A2297DA02B8F4BA8E0
 
 if __name__ == '__main__':
-    y = sm3.sm3_hash(func.bytes_to_list(b"abc"))
+    from gmssl import sm3
+    y = sm3.sm3_hash(b"abc")
     print(y)
--- tests/test_sm4.py
+++ tests/test_sm4.py
@@ -1,18 +1,53 @@
 from gmssl.sm4 import CryptSM4, SM4_ENCRYPT, SM4_DECRYPT
+import random
 
-key = b'3l5butlj26hvv313'
-value = b'111'
-iv = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-crypt_sm4 = CryptSM4()
 
-crypt_sm4.set_key(key, SM4_ENCRYPT)
-encrypt_value = crypt_sm4.crypt_ecb(value)
-crypt_sm4.set_key(key, SM4_DECRYPT)
-decrypt_value = crypt_sm4.crypt_ecb(encrypt_value)
-assert value == decrypt_value
+def test_sm4_cbc(basic_loop, rand_bytes):
+    for _ in range(basic_loop):
+        crypt_sm4 = CryptSM4()
+        msg = rand_bytes(random.randint(1,100))
+        key = rand_bytes(16)
+        iv = rand_bytes(16)
 
-crypt_sm4.set_key(key, SM4_ENCRYPT)
-encrypt_value = crypt_sm4.crypt_cbc(iv , value)
-crypt_sm4.set_key(key, SM4_DECRYPT)
-decrypt_value = crypt_sm4.crypt_cbc(iv , encrypt_value)
-assert value == decrypt_value
+        crypt_sm4.set_key(key, SM4_ENCRYPT)
+        enc = crypt_sm4.crypt_cbc(iv, msg)
+
+        crypt_sm4.set_key(key, SM4_DECRYPT)
+        dec = crypt_sm4.crypt_cbc(iv, enc)
+
+        assert dec == msg
+
+
+def test_sm4_ecb(basic_loop, rand_bytes):
+    for _ in range(basic_loop):
+        crypt_sm4 = CryptSM4()
+        msg = rand_bytes(random.randint(1,100))
+        key = rand_bytes(16)
+        iv = rand_bytes(16)
+
+        crypt_sm4.set_key(key, SM4_ENCRYPT)
+        enc = crypt_sm4.crypt_ecb(msg)
+
+        crypt_sm4.set_key(key, SM4_DECRYPT)
+        dec = crypt_sm4.crypt_ecb(enc)
+
+        assert dec == msg
+
+
+if __name__ == '__main__':
+    key = b'3l5butlj26hvv313'
+    value = b'111'
+    iv = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    crypt_sm4 = CryptSM4()
+
+    crypt_sm4.set_key(key, SM4_ENCRYPT)
+    encrypt_value = crypt_sm4.crypt_ecb(value)
+    crypt_sm4.set_key(key, SM4_DECRYPT)
+    decrypt_value = crypt_sm4.crypt_ecb(encrypt_value)
+    assert value == decrypt_value
+
+    crypt_sm4.set_key(key, SM4_ENCRYPT)
+    encrypt_value = crypt_sm4.crypt_cbc(iv, value)
+    crypt_sm4.set_key(key, SM4_DECRYPT)
+    decrypt_value = crypt_sm4.crypt_cbc(iv, encrypt_value)
+    assert value == decrypt_value
