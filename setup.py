4072cbb917d6b36a06f1216d6f403f45d791edf8
duanhongyi <duanhongyi@doopai.com>
2022-06-21 16:07:10 +0800

    set long_description_content_type

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     long_description=README,
+    long_description_content_type="text/markdown",
     url='https://github.com/duanhongyi/gmssl',
     install_requires=requires,
     tests_require=test_requirements,
